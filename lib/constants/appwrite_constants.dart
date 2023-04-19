6325677bab28223902486d95f7a02d3fcff4a272
elysian12 <elysian1232@gmail.com>
2023-04-19 21:45:09 +0530

    added platform check

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 class AppwriteConstants {
   static const String databaseId = '64400c4d73b96bb634ce';
   static const String projectId = '64400ba975746c3992b5';
-  static const String endPoint = 'http://192.168.1.4:80/v1';
+  static String endPoint =
+      Platform.isAndroid ? 'http://192.168.1.4:80/v1' : 'http://localhost/v1';
 
   static const String userCollection = '64400c5719bf050d1c59';
 }
