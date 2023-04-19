2d9578e7480e6f4dc734ebc6a5273884cf6d8f37
AdityaCubicles45 <67513926+AdityaCubicles45@users.noreply.github.com>
2023-04-19 22:34:58 +0530

    appwrite constants updation

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -1,10 +1,10 @@
 import 'dart:io';
 
 class AppwriteConstants {
-  static const String databaseId = '64400c4d73b96bb634ce';
-  static const String projectId = '64400ba975746c3992b5';
+  static const String databaseId = '64145fffcfebd352d92d';
+  static const String projectId = '64145cf1d95c35817a7e';
   static String endPoint =
-      Platform.isAndroid ? 'http://192.168.1.4:80/v1' : 'http://localhost/v1';
+      Platform.isAndroid ? 'http://192.168.0.102:80/v1' : 'http://localhost/v1';
 
-  static const String userCollection = '64400c5719bf050d1c59';
+  static const String userCollection = '643a9f420595fe8f20e6';
 }
